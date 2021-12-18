397f1d4b3ab8380d76cb93ee5168ccb62bdf25ce
slowpocker <90972217+slowpocker@users.noreply.github.com>
2021-12-18 16:33:43 +0900

    タスク追加機能の実装

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  //テキストボックスの値を取得し。初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //liタグを生成
+  const li = document.createElement("li");
+  // li.innerText = inputText;
+  //divタグを生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+  //liタグの子要素に各要素を設定
+  li.appendChild(div);
+  console.log(div);
+  console.log(li);
+  //pタグを生成
+  const p = document.createElement("p");
+  p.className = "list-item";
+  p.innerText = inputText;
+  //pタグの子要素に各要素を設定
+  div.appendChild(p);
+  alert(inputText);
+
+  document.getElementById("imcomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
